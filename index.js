562cfcc5d13e17f37ce31d6fb852a047039e0d00
Kiril Vatev <vatev.1@gmail.com>
2016-07-17 08:23:11 -0400

    making sure that merging the root rule will always work correctly, even if it was not first

--- index.js
+++ index.js
@@ -20,10 +20,21 @@ function ecToIni(ec) {
 
     return ec.reduce(function(seed, arr) {
         if (arr[0] === null && arr[1] && arr[1].root !== undefined) {
-            return { root: arr[1].root };
+            var newSeed = { root: arr[1].root };
+            
+            // Copy all existing values, to make sure
+            // root is first in the object. Serialize
+            // should take care of this, but we will
+            // do it here for the object anyway.
+            forEach(seed, function(val, key) {
+                newSeed[key] = val;
+            });
+            
+            seed = newSeed;
+        } else if (arr[0] && arr[1]) {
+            seed[arr[0]] = arr[1];
         }
         
-        seed[arr[0]] = arr[1];
         return seed;
     }, {});
 }
@@ -44,15 +55,22 @@ function serialize(obj) {
         if (key === 'root') {
             // add a blank life after
             elems.unshift('');
+            // add root first
             elems.unshift(util.format('%s = %s', key, val));
         } else if (typeof val === 'object') {
+            // add the rule as a title, and serialize
+            // the rest of the object
             elems.push(util.format('[%s]', key));
             elems.push(serialize(val));
         } else {
+            // this is a key value pair, insert normally
             elems.push(util.format('%s = %s', key, val));
         }
     });
     
+    // we could end up with multiple new lines at the
+    // end due to recurssion, so we'll trim that, and
+    // add a single new line
     return elems.join('\n').trim() + '\n';
 }
 
